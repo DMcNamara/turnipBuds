3060c68a7812ec2f565e37fcecd85a818614e920
Dan McNamara <dmcnamara1@gmail.com>
2020-05-01 17:30:14 -0400

    Add a comment to calculate week

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,6 +1,11 @@
 import { formatISO, startOfDay, startOfWeek } from 'date-fns';
 import * as Crypto from 'expo-crypto';
 
+/**
+ * Firestore doesn't like monotonically increasing document IDs. So instead of using the date,
+ * we hash the user id and date
+ * @see https://firebase.google.com/docs/firestore/best-practices?authuser=0#document_ids
+ */
 export async function calculateWeekHash(sunday: Date, uid: string) {
 	const date = formatISO(sunday, {
 		format: 'basic',
